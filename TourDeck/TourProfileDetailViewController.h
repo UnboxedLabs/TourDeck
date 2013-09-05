32da50f938c003d1fdfa7dc9697857bf57270b18
David Small <mrdasmall@gmail.com>
2013-09-05 11:13:27 -0400

    Programmatically added navigation bar to Tour Profile Detail screen.

---
+++ TourDeck/TourProfileDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TourProfileDetailViewController.h
+//  TourDeck
+//
+//  Created by David Small on 9/5/13.
+//  Copyright (c) 2013 Unboxed Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TourProfileDetailViewController : UITableViewController
+
+@end
